74df721d950d2f3eabba5bc3b23275808a45575b
csk6314 <37896060+csk6314@users.noreply.github.com>
2022-02-20 17:51:14 +0900

    Home Slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMovieResult } from "../api";
@@ -15,13 +17,13 @@ const Loader = styled.div`
     align-items: center;
 `;
 
-const Banner = styled.div<{bgPhoto:string}>`
+const Banner = styled.div<{ bgPhoto: string }>`
     height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     padding:60px;
-    background-image: linear-gradient(rgba(20,20,20,0),60%,rgba(20,20,20,1)),url(${(props)=>props.bgPhoto});
+    background-image: linear-gradient(rgba(20,20,20,0),60%,rgba(20,20,20,1)),url(${(props) => props.bgPhoto});
     background-size:cover;
     text-shadow: 1px 1px 8px rgba(20,20,20,1);
 `;
@@ -37,16 +39,83 @@ const Overview = styled.p`
     width: 40%;
 `;
 
+const Slider = styled.div`
+    position: relative;
+    top:-100px;
+    width: 100%;
+    height: 200px;
+    overflow-x:hidden;
+`;
+
+const Row = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(6,1fr);
+    gap:5px;
+    position: absolute;
+    width: 100%;
+`;
+
+const Box = styled(motion.div)<{bgPhoto:string}>`
+    background-color: white;
+    background-image: url(${props=>props.bgPhoto});
+    background-size: cover;
+    height: 200px;
+    color:red;
+    font-size:20px;  
+`;
+
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth + 10
+    },
+    visible: { x: 0 },
+    exit: { x: -window.outerWidth - 10 },
+};
+
 const Home = () => {
     const { data, isLoading } = useQuery<IGetMovieResult>(["movies", "nowPlaying"], getMovies);
     console.log(data);
+    const [index, setIndex] = useState(0);
+    const [leaving,setLeaving] = useState(false);
+    const offset=6;
+    const toggleLeaving = () => {
+        setLeaving((prev) => !prev);
+    }
+    const increaseIndex = () => {
+        if(data) {
+        if(leaving) return;
+        toggleLeaving();
+        const totalMovies = data.results.length -1;
+        const maxIndex = Math.floor(totalMovies/offset) - 1;    
+        setIndex((prev) => (prev===maxIndex?0:prev+1));
+        }
+    };
     return <Wrapper>
         {isLoading ? <Loader>Loading...</Loader> :
             <>
-                <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path||"")}>
+                <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
                     <Title>{data?.results[0].title}</Title>
                     <Overview>{data?.results[0].overview}</Overview>
                 </Banner>
+                <Slider>
+                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                        <Row
+                            variants={rowVariants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="exit"
+                            transition={{type:"tween",duration:1}}
+                            key={index}
+                        >
+                            {data?.results.slice(1).slice(offset*index,offset*index+offset).map((movie)=> (
+                                <Box 
+                                key={movie.id}
+                                bgPhoto={makeImagePath(movie.backdrop_path,"w500")}
+                                >{movie.title}</Box>
+                            ))}
+                        </Row>
+                    </AnimatePresence>
+                </Slider>
             </>
         }
     </Wrapper>
